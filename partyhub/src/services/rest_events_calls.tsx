ad31ec3975ecafb40316208ea13b6cb5ee6e0cc0
Serban Andrei Ionescu <serbanandrei.ionescu@gmail.com>
2022-12-04 19:05:22 +0200

    Remove node.modules

--- partyhub/src/services/rest_events_calls.tsx
+++ partyhub/src/services/rest_events_calls.tsx
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import { Request,Response,NextFunction } from "express";
 import axios, { AxiosResponse } from "axios";
 import { BASE_URL } from "./environment";
 
